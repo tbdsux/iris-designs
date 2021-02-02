45153217fced2204d047871d154e37a72b1dd710
TheBoringDude <iamcoderx@gmail.com>
2021-02-02 22:46:57 +0800

    Remove <Container>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import { AppProps, Container } from 'next/app'
+import { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
 import '../styles/tailwind.css'
 import SEO from '../next-seo.config'
 
 function IrisDesigns({ Component, pageProps }: AppProps) {
   return (
-    <Container>
+    <>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
-    </Container>
+    </>
   )
 }
 
