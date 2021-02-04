e676b941c4c2bc0ea36eef23123ad613c582609a
TheBoringDude <iamcoderx@gmail.com>
2021-02-04 13:21:04 +0800

    Init page contact-us

---
+++ pages/contact-us.tsx
@@ -0,0 +1,14 @@
+import Layout from '../components/Layout'
+import NavMenu from '../components/NavMenu'
+
+const ContactUs = () => {
+  return (
+    <Layout title="Contact Us | Iris Designs">
+      <NavMenu />
+
+      <div>Contact Us</div>
+    </Layout>
+  )
+}
+
+export default ContactUs
